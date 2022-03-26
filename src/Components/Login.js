11f6bc0478b49d1c6d94fe52e906bff3c96b8609
John-09 <johnsd2709@gmail.com>
2022-03-26 09:28:26 +0530

    Updated

---
+++ src/Components/Login.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+//import Form from "react-bootstrap/Form";
+///import Button from "react-bootstrap/Button";
+import "./Login.css";
+
+export default function Login() {
+
+    return(
+      <div id="loginform">
+        <FormHeader title="Login" />
+        <Form />
+      </div>
+    )
+  }
+
+
+const FormHeader = props => (
+    <h2 id="headerTitle">{props.title}</h2>
+);
+
+
+const Form = props => (
+   <div>
+     <FormInput description="Email" placeholder="Enter your email" type="email" />
+     <FormInput description="Password" placeholder="Enter your password" type="password"/>
+     <FormButton title="Log in"/>
+   </div>
+);
+
+const FormButton = props => (
+  <div id="button" class="row">
+    <button>{props.title}</button>
+  </div>
+);
+
+const FormInput = props => (
+  <div class="row">
+    <label>{props.description}</label>
+    <input type={props.type} placeholder={props.placeholder}/>
+  </div>  
+);
